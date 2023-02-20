991d9e2c9e28f5bdc191c2c17c5286293f6ddf68
zaynmalik2611 <zabideen.bscs17seecs@seecs.edu.pk>
2023-02-20 20:44:34 +0500

    chore: add prisma, define schema, define seed.ts

---
+++ prisma/migrations/20230220152958_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      username: 'zain',
+    },
+  });
+
+  const post1 = await prisma.post.create({
+    data: {
+      title: 'First Post',
+      content: 'This is my first post',
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  const post2 = await prisma.post.create({
+    data: {
+      title: 'Second Post',
+      content: 'This is my second post',
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  console.log({ user, post1, post2 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
